2c222a97dfb0c2c4d31f15503164b44baa131409
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2021-11-27 14:50:57 -0300

    Criar informacoes do produto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { GridContainer, Img, ImgWrapper, Wrapper, ContentWrapper, SneakerTitle, ProductTitle, ProductText, PriceContainer, PriceText, DiscountText } from "./components/UI/style";
 import { GlobalStyle } from "./components/GlobalStyle";
 import Header from "./components/Header";
+
+import sneaker from './assets/sneaker02.jpg'
 
 function App() {
   return (
@@ -7,7 +10,26 @@ function App() {
     <GlobalStyle />
     <Header />
     <main>
-      
+      <Wrapper>
+        <GridContainer>
+
+          <ImgWrapper>
+            <Img alt="sneaker" src={sneaker}></Img>
+          </ImgWrapper>
+
+          <ContentWrapper>
+            <SneakerTitle>sneaker company</SneakerTitle>
+            <ProductTitle>Fall Limited Edition Sneakers</ProductTitle>
+            <ProductText>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubbler outer sole. they'll withstand everything the weather can offer.</ProductText>
+
+            <PriceContainer>
+              <PriceText>$125.00</PriceText>
+              <DiscountText>$250.00</DiscountText>
+            </PriceContainer>
+          </ContentWrapper>
+
+        </GridContainer>
+      </Wrapper>
     </main>
     </>
   )
---
+++ src/components/UI/style.js
@@ -0,0 +1,120 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  max-width: 1200px;
+  width: 90%;
+  margin: 0 auto;
+
+  @media screen and (min-width:800px) {
+    padding-top: 3rem;
+  }
+`
+
+export const GridContainer = styled.div`
+  display: grid;
+
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr;
+  @media screen and (min-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+  }
+`
+
+export const ImgWrapper = styled.div`
+  width: 100%;
+  padding-right: 1rem;
+  text-align: center;
+`
+
+export const Img = styled.img`
+  object-fit: cover;
+  object-position: center;
+  width: 100%;
+  height: 100%;
+  border-radius: 5%;
+  max-width: 500px;
+`
+
+export const ContentWrapper = styled.div `
+
+`
+
+export const SneakerTitle = styled.h3`
+  color: orange;
+  font-weight: 600;
+  font-size: 1.25rem;
+  text-transform: uppercase;
+  font-family: "Poppins", sans-serif;
+  margin-top: 2.5rem;
+`
+
+export const ProductTitle = styled.h1`
+  color: #000;
+  font-family: "Poppins", sans-serif;
+  font-size: 3rem;
+  font-weight: bolder;
+  line-height: 1.2;
+  margin-top: 1.5rem;
+`
+
+export const ProductText = styled.p`
+  font-size: 1.25rem;
+  font-family: "Poppins", sans-serif;
+  color: #999999;
+  margin-top: 2rem;
+  font-weight: 500;
+`
+
+export const PriceContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 1rem;
+
+  @media screen and (min-width: 800px) {
+    flex-direction: column;
+    align-items: initial;
+    justify-content: initial;
+  }
+`
+
+export const PriceText = styled.span `
+  color: #000;
+  font-size: 2rem;
+  font-weight: bolder;
+  font-family: "Poppins",sans-serif;
+  position: relative;
+  display: inline-block;
+  max-width: 100px;
+
+  &::before {
+    content: '50%';
+    width: 50px;
+    background-color: #ffecc8;
+    color: #000;
+    position: absolute;
+    top: 50%;
+    right: -80%;
+    font-size: 1.25rem;
+    color: orange;
+    text-align: center;
+    border-radius: 10%;
+    transform: translateY(-50%);
+  }
+
+  @media screen and (min-width:800px) {
+    &::before {
+      right: -100%;
+    }
+  }
+`
+
+export const DiscountText = styled.span`
+  color: lightgray;
+  font-size: 1.5rem;
+  text-decoration: line-through;
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+`
